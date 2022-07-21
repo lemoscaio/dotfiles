2ecee869eed57c3aeb85f4561efacb6320ebda97
lemoscaio <caio_lemos94@hotmail.com>
2022-07-20 22:36:50 -0300

    add tmux and ohmyzsh config

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -31,13 +31,13 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory
 # Alias
 Set-Alias -Name vim -Value nvim
 Set-Alias ll ls
-Set-Alias g git
-Set-Alias grep findstr
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+#Set-Alias g git
+#Set-Alias grep findstr
+#Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+#Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
 # Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
+#function which ($command) {
+#  Get-Command -Name $command -ErrorAction SilentlyContinue |
+#    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+#}
